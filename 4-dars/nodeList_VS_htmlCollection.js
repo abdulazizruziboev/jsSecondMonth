5747caeca919b6110bd688317b74f050d8b4e837
abdulaziz <telegrampreimum4@gmail.com>
2025-12-09 18:19:13 +0500

    New Folder

---
+++ 4-dars/nodeList_VS_htmlCollection.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////////
+console.log("=====================================================")
+const listElementsHTML = document.getElementsByClassName("fruits"); // No-Iterable
+const listElementsNODE = document.querySelectorAll(".fruits"); // Iterable | Faqat forEach bilan!
+console.log(listElementsHTML); // HTML Collection
+console.log(listElementsNODE); // Node List
+/////////////////////////////////////////////////////////////////////////////////////
+console.log("=====================================================")
+listElementsNODE.forEach(el => {
+    el.style.color = "red";
+    console.log(el.textContent);
+    }
+);
+console.log("=====================================================")
+/////////////////////////////////////////////////////////////////////////////////////
+// HTML Collections faqat HTML elementlarni oladi.
+/////////////////////////////////////////////////////////////////////////////////////
+// Node esa barchasini: comment, text, HTML elementlarini va hokazolarni o'z ichiga oladi.
+/////////////////////////////////////////////////////////////////////////////////////
+const listElementsHTML2 = document.querySelector(".fruits"); // No-Iterable
+const listElementsNODE2 = document.querySelector(".fruits"); // Iterable | Faqat forEach bilan!
+console.log(listElementsHTML2.children); // Faqat teg ichiga o'ralganlarni oladi
+console.log(listElementsNODE2.childNodes); // Barchasini oladi
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+// HTML Collections faqat HTML elementlarni oladi.
+/////////////////////////////////////////////////////////////////////////////////////
+// Node esa barchasini: comment, text, HTML elementlarini va hokazolarni o'z ichiga oladi.
+/////////////////////////////////////////////////////////////////////////////////////
+console.log("=====================================================")
+console.log(listElementsHTML.length);
+console.log(listElementsNODE.length);
+const newChild = document.createElement("span");
+newChild.classList.add("fruits");
+newChild.textContent="Anjir"
+document.querySelector(".listFruits").appendChild(newChild);
+console.log(listElementsHTML.length); // Elementlar Ro'yxati O'zgardi
+console.log(listElementsNODE.length); // O'zgarishi mumkin aksariyat xolarda 
+console.log(document.querySelector(".listFruits").childNodes); // Bu methoda live o'zgarish mavjud yani DOM o'zgarsa bu ham o'zgaradi.
