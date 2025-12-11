2f220caf447d848ad2116295c431b13da24b920d
abdulaziz <telegrampreimum4@gmail.com>
2025-12-11 19:49:04 +0500

    Error

--- 5-dars/football/script.js
+++ 5-dars/football/script.js
@@ -1,13 +1,19 @@
+const kickSound = new Audio();
+kickSound.src = "./ball_kick.mp3";
 const wrapper = document.getElementById("wrapper");
 const ball = document.getElementById("ball");
 
 wrapper.addEventListener("click", (evt) => {
-  const rect = wrapper.getBoundingClientRect();
-  const x = evt.clientX - rect.left; // X relative to wrapper
-  const y = evt.clientY - rect.top;  // Y relative to wrapper
-
+  kickSound.play();
+  let r=ball.clientWidth/2;
+  let ww=wrapper.clientWidth;
+  let wh=wrapper.clientHeight;
+  let cy = evt.offsetY;
+  let cx = evt.offsetX;
+  let fixedY=Math.max(r,Math.min(wh-r,cy));
+  let fixedX=Math.max(r,Math.min(ww-r,cx));
   setTimeout(() => {
-    ball.style.top = `${y}px`;
-    ball.style.left = `${x}px`;
+    ball.style.top = `${fixedY}px`;
+    ball.style.left = `${fixedX}px`;
   }, 300);
 });
